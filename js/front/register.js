6e0dd7d008bccfad10ec4b94095cd5476f10e30b
Abrar Ur Alam <safee20142@gmail.com>
2024-05-30 00:48:56 +0600

    added input validation to regiserPage.html via register.js + organised folders

---
+++ js/front/register.js
@@ -0,0 +1,196 @@
+
+
+
+// DRY code
+function hide_element(element_id){
+    document.getElementById(element_id).setAttribute("hidden", "true");
+}
+function unhide_element(element_id){
+    document.getElementById(element_id).removeAttribute("hidden");
+}
+
+
+// input validation function
+function validate_registration(){
+
+    check_first_name();
+    check_last_name();
+    check_gender();
+    check_dob();
+    check_email();
+    check_password();
+    check_retyped_password();
+}
+
+
+
+function check_first_name(){
+    let first_name = document.getElementById("fname").value;
+    console.log("first name = " + first_name);
+
+    let prompt = document.getElementById("fname_prompt");
+    if(first_name == ""){
+        prompt.innerHTML = "This field must be filled";
+    }
+    else if(first_name.match("^.*[0-9].*$")){
+        prompt.innerHTML = "Names cannot have numbers in them";
+    }
+    else{
+        hide_element(prompt.id);
+        return true;
+    }
+    unhide_element(prompt.id);
+    return false;
+}
+
+
+
+function check_last_name(){
+    let last_name = document.getElementById("lname").value;
+
+    let prompt = document.getElementById("lname_prompt");
+    if(last_name == ""){
+        prompt.innerHTML = "This field must be filled";
+    }
+    else if(last_name.match("^.*[0-9].*$")){
+        prompt.innerHTML = "Names cannot have numbers in them";
+    }
+    else{
+        hide_element(prompt.id);
+        return true;
+    }
+    unhide_element(prompt.id);
+    return false;
+}
+
+
+
+function check_gender(){
+    let gender = document.getElementById("gender").value;
+
+    let prompt = document.getElementById("gender_prompt");
+    if(gender == ""){
+        unhide_element(prompt.id);
+        prompt.innerHTML = "Must select a gender"
+        return false;
+    }
+    else{
+        hide_element(prompt.id);
+        return true;
+    }
+}
+
+function check_dob(){
+    let dob = new Date(Date.parse(document.getElementById("dob").value));
+    let age = new Date().getFullYear() - dob.getFullYear();
+
+    let prompt = document.getElementById("dob_prompt");
+    if(age < 10){
+        prompt.innerHTML = "Must be above 10 years of age";
+    }
+    else if(dob == "Invalid Date"){
+        prompt.innerHTML = "Must select a date of birth";
+    }
+    else{
+        hide_element(prompt.id);
+        return true;
+    }
+    unhide_element(prompt.id);
+    return false;
+}
+
+function check_email(){
+    let email = document.getElementById("email").value;
+
+    let prompt = document.getElementById("email_prompt");
+    if(email.toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)){
+        hide_element(prompt.id);
+        return true;
+    }
+    else if(email == ""){
+        prompt.innerHTML = "This field must be filled";
+    }
+    else{
+        prompt.innerHTML = "Invalid email address"
+    }
+    unhide_element(prompt.id)
+    return false;
+}
+
+
+
+function check_password(){
+    let password = document.getElementById("password").value;
+
+    let prompt = document.getElementById("password_prompt");
+    prompt.innerHTML = ""; // clear up the innerHTML so that previous prompts are reset
+    if(password == ""){
+        unhide_element(prompt.id);
+        prompt.innerHTML = "This field must be filled";
+        return false;
+    }
+    
+    
+    let min_length_fulfilled = (password.length >= 5);
+    let symbol_used = false;
+    let number_used = false;
+    let no_whitepaces = true;
+  
+    for(let char of password){
+        let ascii_char_number = char.charCodeAt(0);
+        if(char.match(/\s/)){ // checks for whitespace (password cannot have spaces)
+            no_whitepaces = false;
+        }
+        else if(ascii_char_number >= 48 && ascii_char_number <= 57){
+            number_used = true;
+        }
+        else if(
+            (ascii_char_number >= 33 && ascii_char_number <= 47) ||
+            (ascii_char_number >= 58 && ascii_char_number <= 64) ||
+            (ascii_char_number >= 91 && ascii_char_number <= 96) ||
+            (ascii_char_number >= 123 && ascii_char_number <= 126)){
+            symbol_used = true;
+        }
+    }
+  
+    if(!min_length_fulfilled){
+        prompt.innerHTML += "Password length must be at least 5\n";
+    }
+    if(!no_whitepaces){
+        prompt.innerHTML += "Password cannot have whitespaces\n";
+    }
+    if(!symbol_used){
+        prompt.innerHTML += "Password must have at least one symbol\n";
+    }
+    if(!number_used){
+        prompt.innerHTML += "Password must have at least one number\n";
+    }
+
+    if (min_length_fulfilled && no_whitepaces && symbol_used && number_used){
+        hide_element(prompt.id);
+        return true;
+    }
+    else{
+        unhide_element(prompt.id);
+        return false;
+    }
+  }
+
+  function check_retyped_password(){
+    let retyped_password = document.getElementById("retype_password").value;
+    let password = document.getElementById("password").value;
+
+    let prompt = document.getElementById("retype_password_prompt");
+    if(retyped_password == ""){
+        prompt.innerHTML = "This field must be filled";
+    }
+    else if(retyped_password != password){
+        prompt.innerHTML = "Retyped password did not match set password"
+    }
+    else{
+        hide_element(prompt.id);
+        return true;
+    }
+    unhide_element(prompt.id);
+    return false;
+  }
